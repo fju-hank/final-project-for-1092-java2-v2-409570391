d7779217d6f43f8023a29b708187dc7e4d793b12
王冠棋 <coco82401@gmail.com>
2021-06-22 14:23:18 +0800

    6/22 14:23

--- src/main/java/test/Try.java
+++ src/main/java/test/Try.java
@@ -3,44 +3,68 @@ import java.util.*;
 public class Try {
     public static void main(String args[]){
 
-        Scanner keyin = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        final int a=8, b=6, c=4, d=3;
+        final int CP = 5 , FP = 4 ,AP = 5 , SP = 3 , EP = 2 ,SLP = 3 ,BTP = 4 , BP = 2 , UP = 3 ,DP = 5 ;
 
-        int weights = a+b+c+d;
+        int weights = CP+FP+AP+SP+EP+SLP+BTP+BP+UP+DP;
 
-        int x, y, z, k;
+        int a,b ,c ,d ,e,f,g,h,i,j;
 
         float value;
 
-        System.out.printf("*** 新冠肺炎自我診斷系統  ***\n");
+        System.out.printf("Depression self-diagnosis system\n");
 
-        System.out.printf("請輸入各種症狀明顯度如何 (0 ~ 10)\n");
+        System.out.printf("Please enter how obvious the various symptoms are(0-10)\n");
 
-        System.out.printf("\t如: 不明顯(0),稍明顯 (4) 明顯(6),很明顯(10)\n");
-
-
-
-        System.out.printf("請輸入發燒症狀明顯否(0~10) =>");
-
-        x = keyin.nextInt();
-
-        System.out.print("請輸入筋骨酸痛症狀明顯否(0~10) =>");
-
-        y = keyin.nextInt();
-
-        System.out.print("請輸入感冒症狀明顯否(0~10) =>");
-
-        z = keyin.nextInt();
-
-        System.out.print("請輸入嗜睡症狀明顯否(0~10) =>");
-
-        k = keyin.nextInt();
+        System.out.printf("\tSuch as: not obvious (0), slightly obvious (4) obvious (6), very obvious (10)\n");
 
 
 
-        value = (float)(x*a + y*b +z*c + k*d)/weights;
+        System.out.printf("I often feel like crying(0~10)\n =>");
 
+        a = scanner.nextInt();
+
+        System.out.print("I feel bad(0~10)\n =>");
+
+        b = scanner.nextInt();
+
+        System.out.print("I feel tantrums more easily than before(0~10)\n =>");
+
+        c = scanner.nextInt();
+
+        System.out.print("I can't sleep well(0~10)\n =>");
+
+        d = scanner.nextInt();
+
+        System.out.println("I don't feel like eating\n =>");
+
+        e = scanner.nextInt();
+
+        System.out.println("I think thinking or doing things is slower than before\n =>");
+
+        f = scanner.nextInt();
+
+        System.out.println("I'm more likely to think in the bad direction\n =>");
+
+        g = scanner.nextInt();
+
+        System.out.println("I feel sick\n =>");
+
+        h = scanner.nextInt();
+
+        System.out.println("I feel very useless\n =>");
+
+        i = scanner.nextInt();
+
+        System.out.println("I can't think about it, I even want to die\n =>");
+
+        j = scanner.nextInt();
+
+
+
+        value = (float)(a*CP+b*FP+c*AP+d*SP+f*EP+g*SLP+h*BP+i*UP+j*DP)/weights;
+if (value)
         //value=Math.sqrt((a*x*x + b*y*y + c*z*z + d*k*k) /(double)(a+b+c+d));
 
         System.out.printf("\n您感染新冠肺炎指數為(0 ~ 10)= %.2f\n", value);
